29eed3da9b80b145734e3d2c2c22d4bd9effbe3d
iarigby <marvinzem@gmail.com>
2021-10-31 15:29:47 +0400

    add tests description

---
+++ instructions.js
@@ -0,0 +1,5 @@
+const webeditor = `
+    <div>
+        
+    </div>
+`
--- tests.js
+++ tests.js
@@ -1,19 +1,46 @@
 const { expect, assert } = chai
 
-// todo rewrite description
-CONFIG.isStep(1) && describe("step 1. Login?? ", () => {
-    setTimeout(() => {
+// აქ ჩამოვწერთ რომელი მერამდენე იყოს რომ შემთხვევით
+// რიცხვების შეცვლა არ დაგვავიწყდეს
+const steps = {
+    setup_and_password: 1,
+    header: 2,
 
-    }, 1000)
-    it("Name provided", () => {
-        const password = document.getElementById('password-input')
-            // .querySelector('.password')
-        console.log(password)
-        console.log('rati')
-        expect(true).eql(true)
+}
+
+
+CONFIG.isStep(steps.setup_and_password) && describe("", () => {
+    /**
+     * აქ უნდა გამოჩნდეს (დინამიურად დაემატოს ინსტრუქციები:
+     *     - code ედიტორის ინსტრუქცია
+     *     - inspect element -ის ინსტრუქცია
+     *     - პირველ ჩატვირთვაზე გენერირდეს პაროლი და შეინახოს CONFIG - ში. 
+     *      ანუ ყველას თავისი ექნება, ერთმანეთს რომ არ გაუზიარონ.
+     *     - დაემატოს div, რომლის id არის find password here. ამ დივში დაემატოს 100-მდე პარაგრაფი
+     *    (კლასი fake-password)  ყველაში იქნება რენდომ რიცხვი. 
+     * რომელიღაცაში (0-100ში რენდომ შერჩეული PASSWORD P INDEX)
+     * იქნება "ნამდვილი" პაროლი (ელემენტს ექნება კლასი real-password), 
+     * ინსტრუქციაში უნდა დაეწეროს, რომ იპოვონ
+     * 
+     */
+    it(`გახსენი html ფაილი კოდის ედიტორში, მოძებნე ხაზი, რომელზეც div#write-password-here
+    არის და ჩაწერე იქ პაროლი`, () => {
+        const n = Number(document.querySelector('div#write-password-here').innerText)
+        expect(n).eql(CONFIG.password)
     })
-    it("Password found", () => {
-        expect(false).eql(true)
+})
+// todo rewrite description
+CONFIG.isStep(steps.header) && describe(`header`, () => {
+    /**
+     * ყველა მათგანი მოწმდება ცალკე ტესტით
+     * სათაური
+     * მენიუსავით რამე div ზემოთ
+     */
+    it(``, () => {
+        
+    })
+    it(``, () => {
+
     })
 })
 
