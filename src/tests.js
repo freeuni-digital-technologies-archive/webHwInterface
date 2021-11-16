5b29d29646d073a5b18fcd8cf36113b3b8f0203f
iarigby <marvinzem@gmail.com>
2021-11-16 20:29:38 +0400

    start hw6

--- src/tests.js
+++ src/tests.js
@@ -1,5 +1,1 @@
-import { expect, assert } from 'chai'
-// აქ ჩამოვწერთ რომელი მერამდენე იყოს რომ შემთხვევით
-// რიცხვების შეცვლა არ დაგვავიწყდეს
-import { toggle } from './utils'
-import { showMessage } from './instructions'
+homeworks/hw6/tests.js
