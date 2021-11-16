11ce2ca698bcf4ff6bc40c8f10686c40cf8e020c
iarigby <marvinzem@gmail.com>
2021-11-16 20:27:25 +0400

    small restructure

---
+++ src/tests.js
@@ -0,0 +1,5 @@
+import { expect, assert } from 'chai'
+// აქ ჩამოვწერთ რომელი მერამდენე იყოს რომ შემთხვევით
+// რიცხვების შეცვლა არ დაგვავიწყდეს
+import { toggle } from './utils'
+import { showMessage } from './instructions'
