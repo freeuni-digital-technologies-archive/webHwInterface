f4bb41d5da3560cb49c5dd25fc83740aadd798c5
iarigby <marvinzem@gmail.com>
2021-11-21 22:28:51 +0400

    hw6

--- homeworks/hw6/index.js
+++ homeworks/hw6/index.js
@@ -1,11 +1,12 @@
 
-/**
-TASK 1
-**/
-function task1() {
-		
-}
-
-function create() {
-	
+function getCalculatorElement() {
+	return `
+		<div id="calculator">
+			<input id="number1">
+			<input id="number2">
+			<button id="add">+</button>
+			<button id="substract">-</button>
+			<span id="result"></span>
+		</div>
+	`
 }
