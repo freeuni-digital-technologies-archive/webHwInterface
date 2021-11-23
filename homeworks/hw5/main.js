0e68944815de7492fad3c06e63d62463f58e3003
iarigby <marvinzem@gmail.com>
2021-11-23 13:52:56 +0400

    remove everything from dist branch

--- homeworks/hw5/main.js
+++
@@ -1,9 +0,0 @@
-import { setupInterface } from '../../src/setup'
-import { generateTests } from './tests'
-
-const { CONFIG, HW, mocha } = setupInterface()
-
-mocha.setup('bdd')
-generateTests(CONFIG)
-HW.launchTests(mocha)
-
--- scripts/createZips.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-rm docs/*/*.zip
-for hw in $(/bin/ls homeworks)
-do 
-	zip -rj docs/$hw/$hw.zip homeworks/$hw/ -x \
-		\*solution\* \*server.js \*extras.js \*tests.js \*main.js
-done
