823d033804a5bbfe7ec184aa6bfe2f96437f533d
iarigby <marvinzem@gmail.com>
2021-11-23 13:51:34 +0400

    publish script

---
+++ scripts/publishTestFiles.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+changes=$(git status --porcelain)
+echo $changes
+if [[ ! -z $changes ]]
+then
+	echo "changes exist in repository. commit first"
+	exit 1
+fi
+
+
+current_branch=$(git branch --show-current)
+rm -r dist 2> /dev/null
+if [[ $2 == "zip" ]]
+then
+	yarn createZips
+fi
+# make sure changes are synced first
+git checkout dist
+git pull
+# go back to build from source
+git checkout $current_branch
+yarn build-student-tests
+git checkout dist
+cp -r dist/* docs/
+git add docs/
+git commit -m "auto commit"
+git push
+git checkout $current_branch
+
