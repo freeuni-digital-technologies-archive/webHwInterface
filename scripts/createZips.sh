0f1a3b84a4c1e3fea6e1f4ba4471eeefd42e0274
iarigby <marvinzem@gmail.com>
2021-12-20 21:20:04 +0400

    anim script

--- scripts/createZips.sh
+++ scripts/createZips.sh
@@ -5,5 +5,5 @@ for hw in $(/bin/ls homeworks)
 do
 	mkdir -p dist/$hw
 	zip -rj dist/$hw/$hw.zip homeworks/$hw/ -x \
-		\*solution\* \*server.js \*extras.js \*tests.js \*main.js
+		\*solution\* \*server.js \*extras.js \*tests.js \*main.js \*animation.js
 done
