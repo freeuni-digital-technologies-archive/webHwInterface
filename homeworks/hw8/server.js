9d04dc7fdfb4adef7769458e49b47dba94ecb027
iarigby <marvinzem@gmail.com>
2021-12-27 14:37:48 +0400

    hw8 server

---
+++ homeworks/hw8/server.js
@@ -0,0 +1,9 @@
+import { setupServer } from '../../src/serverSetup.js'
+import { generateTests } from './tests'
+import { Config } from '../../src/config'
+import { createServerConfig, getScore, generateServerTests } from './extras'
+
+let c = createServerConfig(new Config())
+const { CONFIG, HW, mocha, launch } = setupServer(c)
+generateServerTests(CONFIG)
+launch(getScore)
